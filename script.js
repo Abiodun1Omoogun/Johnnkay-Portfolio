3bfcf65fea3bd6d3ff074bc2032560075f52ef78
Emmanuel Omoogun <abiodunebun12@gmail.com>
2022-08-20 09:56:29 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function myFunction() {
-    var x = document.getElementById("myTopNav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("myTopNav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
 
-$(document).ready(function () {
-    $("#link").click(function (e) {
-        e.preventDefault();
-        window.location.href = "File/randomfile.docx";
-    });
-});
+// $(document).ready(function () {
+//     $("#link").click(function (e) {
+//         e.preventDefault();
+//         window.location.href = "File/randomfile.docx";
+//     });
+// });
