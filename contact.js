5336c7749aabce4236b2a769c3faca71813ab2fe
Emmanuel Omoogun <abiodunebun12@gmail.com>
2022-08-24 12:09:03 +0100

    initial commit

--- contact.js
+++ contact.js
@@ -1,38 +1,43 @@
-// Name 
-let Name = document.getElementById("name");
+function getContactInfo() {
+    // Name 
+    let Name = document.getElementById("name");
 
-if (Name === null){
-    return;
-}
-
-function validateName(Name){
-    if(Name.length < 10 && Name.length > 0){
-        return true;
-    }else{
-        return false;
+    if (Name === null){
+        return;
     }
-}
-while(validateName(Name) === false){
- Name = prompt("Name must be less than 10 and greater than 0")
-}
 
-
-// Email address
-let email = document.getElementById("email");
-
-if (email === null){
-    return;
-}
-
-function validateEmail(email){
-    var emailCheck =  /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    emailCheckResult = emailCheck.test(email);
-    if (emailCheckResult === true){
-        return true;
-    }else{
-        return false;
+    function validateName(Name){
+        if(Name.length < 10 && Name.length > 0){
+            return true;
+        }else{
+            return false;
+        }
     }
-}
-while(validateEmail(email) === false){
-    email = prompt("Enter a valid email")
+    while(validateName(Name) === false){
+    Name = prompt("Name must be less than 10 and greater than 0")
+    }
+
+
+    // Email address
+    let email = document.getElementById("email");
+
+    if (email === null){
+        return;
+    }
+
+    function validateEmail(email){
+        var emailCheck =  /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        emailCheckResult = emailCheck.test(email);
+        if (emailCheckResult === true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    while(validateEmail(email) === false){
+        email = prompt("Enter a valid email")
+    };
+
 };
+
+getContactInfo();
