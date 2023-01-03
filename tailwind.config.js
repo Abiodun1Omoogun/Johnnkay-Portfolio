f73a660d4bcb91a1c312704b7c6328e5aaf4c64f
Emmanuel Omoogun <abiodunebun12@gmail.com>
2023-01-03 10:53:50 +0100

    create server.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          black: '#000',
-          gray: 'gray',
-          transparent: 'trannsparent',
-        },
-        fontFamily: {
-          san: ['Poppins', 'san-serif'],
-        },
+tailwind.config  = {
+  theme: {
+    extend: {
+      colors: {
+        black: '#000',
+        gray: 'gray',
+        transparent: 'trannsparent',
+      },
+      fontFamily: {
+        san: ['Poppins', 'san-serif'],
+      },
     }
   }
 } 
