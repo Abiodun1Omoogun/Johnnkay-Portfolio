8921135f8838f7c31d6c153988ef28acf59b7c45
Emmanuel Omoogun <abiodunebun12@gmail.com>
2023-01-14 13:21:49 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ tailwind.config  = {
       colors: {
         black: '#000',
         gray: '#404040',
+        lightgray: '#C4C4C4',
         transparent: 'trannsparent',
         purewhite: 'rgba(255, 255, 255, 1)',
       },
